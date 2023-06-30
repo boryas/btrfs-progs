45d4cdb69ba693c65c8aa8eb62f6cc695ea17d09
Boris Burkov <boris@bur.io>
2023-06-30 00:09:37 +0000

    btrfs-progs: tree-checker: handle owner ref items
    Add the new OWNER_REF inline items to the tree-checker extent item
    checking code. We could somehow validate the root id for being a valid
    fstree id, but just skipping it seems fine as well.
    
    Signed-off-by: Boris Burkov <boris@bur.io>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>

--- kernel-shared/tree-checker.c
+++ kernel-shared/tree-checker.c
@@ -1477,6 +1477,8 @@ static int check_extent_item(struct extent_buffer *leaf,
 			}
 			inline_refs += btrfs_shared_data_ref_count(leaf, sref);
 			break;
+		case BTRFS_EXTENT_OWNER_REF_KEY:
+			break;
 		default:
 			extent_err(leaf, slot, "unknown inline ref type: %u",
 				   inline_type);
